6e6b61b87a74968a67b76d6e51e59973bf121680
Lim Young <lim.young@foxmail.com>
2025-06-06 01:40:19 +0800

    Add ApplySkeletalMeshDestruction AnimNotify

---
+++ Source/SkeletalMeshDestruction/Private/AnimNotify/AnimNotify_ApplySkeletalMeshDestruction.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright Lim Young.
+
+
+#include "AnimNotify/AnimNotify_ApplySkeletalMeshDestruction.h"
+
+void UAnimNotify_ApplySkeletalMeshDestruction::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                                      const FAnimNotifyEventReference& EventReference)
+{
+	const AActor* Owner = MeshComp->GetOwner();
+	if (!IsValid(Owner))
+	{
+		return;
+	}
+
+	USkeletalMeshDestructionComponent* DestructionComponent = Owner->FindComponentByClass<
+		USkeletalMeshDestructionComponent>();
+	if (!IsValid(DestructionComponent))
+	{
+		return;
+	}
+
+	if (bApplyDLOD && ApplyDLODConfigs.Num() > 0 && DestructionComponent->IsUseDLOD())
+	{
+		for (auto ApplyDLODConfig : ApplyDLODConfigs)
+		{
+			DestructionComponent->SetDLODByBoneName(ApplyDLODConfig.Key, ApplyDLODConfig.Value);
+		}
+	}
+
+	if (bApplyDismemberment && ApplyDismembermentConfigs.Num() && DestructionComponent->IsUseDismemberment())
+	{
+		for (auto ApplyDismembermentConfig : ApplyDismembermentConfigs)
+		{
+			DestructionComponent->ApplyDismemberment(ApplyDismembermentConfig.Key, ApplyDismembermentConfig.Value);
+		}
+	}
+}
---
+++ Source/SkeletalMeshDestruction/Public/AnimNotify/AnimNotify_ApplySkeletalMeshDestruction.h
@@ -0,0 +1,36 @@
+﻿// Copyright Lim Young.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SkeletalMeshDestructionComponent.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_ApplySkeletalMeshDestruction.generated.h"
+
+/**
+ * 
+ */
+UCLASS(DisplayName = "Apply SKM Destruction")
+class SKELETALMESHDESTRUCTION_API UAnimNotify_ApplySkeletalMeshDestruction : public UAnimNotify
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Skeletal Mesh Destruction|DLOD")
+	bool bApplyDLOD = false;
+
+	UPROPERTY(EditAnywhere, Category = "Skeletal Mesh Destruction|DLOD",
+		meta = (EditCondition = "bApplyDLOD", EditConditionHides, ForceInlineRow))
+	TMap<FName, uint8> ApplyDLODConfigs;
+
+	UPROPERTY(EditAnywhere, Category = "Skeletal Mesh Destruction|Dismemberment")
+	bool bApplyDismemberment = false;
+
+	UPROPERTY(EditAnywhere, Category = "Skeletal Mesh Destruction|Dismemberment",
+		meta = (EditCondition = "bApplyDismemberment", EditConditionHides, ForceInlineRow))
+	TMap<FName, FDismembermentConfig> ApplyDismembermentConfigs;
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	                    const FAnimNotifyEventReference& EventReference) override;
+};
